731151584c945f54de6668a6163a9f48f8c43f71
Kaiz <84890519+Kaiz404@users.noreply.github.com>
2024-08-18 03:43:30 +0800

    commented out everything

--- app/lib/GreenToken.tsx
+++ app/lib/GreenToken.tsx
@@ -1,15 +1,15 @@
-import { useWallet, InputTransactionData } from "@aptos-labs/wallet-adapter-react";
-import { Aptos, AptosConfig, Network, InputViewFunctionData } from "@aptos-labs/ts-sdk";
+// import { useWallet, InputTransactionData } from "@aptos-labs/wallet-adapter-react";
+// import { Aptos, AptosConfig, Network, InputViewFunctionData } from "@aptos-labs/ts-sdk";
 
-const aptosConfig = new AptosConfig({ network: Network.TESTNET });
-const aptos = new Aptos(aptosConfig);
+// const aptosConfig = new AptosConfig({ network: Network.TESTNET });
+// const aptos = new Aptos(aptosConfig);
 
-const greenToken = '0x7996e8716fb67da48d174d6e9a1bf2517e8ab37fac63a3af1f42f4e3b5644a1c::green_token::GreenToken'
-const greenTokenStore = `0x1::coin::CoinStore<${greenToken}>`;
-const aptosContract = '0x7996e8716fb67da48d174d6e9a1bf2517e8ab37fac63a3af1f42f4e3b5644a1c'
-const storageAccount = '0x7996e8716fb67da48d174d6e9a1bf2517e8ab37fac63a3af1f42f4e3b5644a1c'
+// const greenToken = '0x7996e8716fb67da48d174d6e9a1bf2517e8ab37fac63a3af1f42f4e3b5644a1c::green_token::GreenToken'
+// const greenTokenStore = `0x1::coin::CoinStore<${greenToken}>`;
+// const aptosContract = '0x7996e8716fb67da48d174d6e9a1bf2517e8ab37fac63a3af1f42f4e3b5644a1c'
+// const storageAccount = '0x7996e8716fb67da48d174d6e9a1bf2517e8ab37fac63a3af1f42f4e3b5644a1c'
 
-const { connected, isLoading, account, network, wallet, wallets, connect, disconnect, signTransaction, submitTransaction, signAndSubmitTransaction, signMessage, signMessageAndVerify, changeNetwork } = newFunction();
+// const { connected, isLoading, account, network, wallet, wallets, connect, disconnect, signTransaction, submitTransaction, signAndSubmitTransaction, signMessage, signMessageAndVerify, changeNetwork } = newFunction();
 
 
 // const checkAccountInitialized = async () =>{
